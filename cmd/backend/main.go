e994a08a0db1494c3dff2877f27dca5e34c982d5
aknea001 <aksel.nergaard@outlook.com>
2025-09-23 14:57:34 +0200

    added a validate token endpoint

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/aknea001/goDoList/pkg"
@@ -60,12 +61,13 @@ func main() {
 
 		jwtKey := []byte(os.Getenv("jwtKey"))
 
-		// replace with IDs when proper DB implemented
-		baseToken := jwt.NewWithClaims(jwt.SigningMethodHS256,
-			jwt.MapClaims{
-				"username": UserData.Username,
-				"exp":      time.Now().Add(1 * time.Hour),
-			})
+		// replace sub with IDs when proper DB implemented
+		claims := &jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(1 * time.Hour)),
+			Subject:   UserData.Username,
+		}
+
+		baseToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 		signedToken, err := baseToken.SignedString(jwtKey)
 		if err != nil {
@@ -75,6 +77,51 @@ func main() {
 		ctx.JSON(200, gin.H{
 			"msg":   "Success",
 			"token": signedToken,
+		})
+	})
+
+	router.GET("/validateToken", func(ctx *gin.Context) {
+		headers := ctx.Request.Header
+
+		auth := headers.Get("Authorization")
+		if auth == "" {
+			ctx.JSON(401, gin.H{
+				"msg": "Missing authorization of type Bearer",
+			})
+			return
+		}
+
+		authSplit := strings.Split(auth, " ")
+		if len(authSplit) <= 1 || authSplit[0] != "Bearer" {
+			ctx.JSON(401, gin.H{
+				"msg": "Missing authorization of type Bearer",
+			})
+
+			return
+		}
+
+		signedToken := authSplit[1]
+		validMethods := make([]string, 1)
+		validMethods[0] = "HS256"
+
+		token, err := jwt.Parse(signedToken, func(token *jwt.Token) (any, error) {
+			return []byte(os.Getenv("jwtKey")), nil
+		}, jwt.WithValidMethods(validMethods))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !token.Valid {
+			ctx.JSON(401, gin.H{
+				"msg": "Invalid authorization",
+			})
+
+			return
+		}
+
+		ctx.JSON(200, gin.H{
+			"msg": "Token is valid",
 		})
 	})
 
